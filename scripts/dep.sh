c7b0ab1d0609b2c3d77ce3b96c03a90937091e97
Thomas Han <thomas.han@live.com>
2015-09-30 14:03:07 +1000

    initial vagrant box

---
+++ scripts/dep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Setup the the box. This runs as root
+
+apt-get -y update
+
+apt-get -y install curl indicator-multiload alien libaio1 htop git yakuake sqlite3 vim wget aria2 aptitude
+
+# You can install anything you need here.
